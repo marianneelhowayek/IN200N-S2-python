53d077fae3e70b2b1e7da7b4efe3fda8824536e8
Marianne <marianneelhowayek@gmail.com>
2025-03-03 16:03:53 +0100

    td2

---
+++ TD/TD2/debut.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+
+# Début de votre code : horizontale
+# x0 = 100
+# x1 = CANVAS_WIDTH - 100
+# y = CANVAS_HEIGHT / 2
+# canvas.create_line(x0, y, x1, y)
+# canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
+# canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
+# canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
+
+# Début de votre code : verticale
+y0 = 100
+y1 = CANVAS_HEIGHT - 100
+x = CANVAS_WIDTH / 2
+canvas.create_line(x, y0, x, y1)
+canvas.create_oval(x - 50, y0 + 50, x + 50, y0 - 50)
+canvas.create_oval(x - 50, y1 + 50, x + 50, y1 - 50)
+canvas.create_oval(x + 50, (y0 + y1) / 2 - 50, x - 50, (y0 + y1) / 2 + 50)
+
+# Fin de votre code
+
+canvas.grid(row= 0, column= 0)
+root.mainloop()
---
+++ TD/TD2/dessin.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+racine = tk.Tk()
+racine.title("Mon dessin")
+
+canvas = tk.Canvas(racine, bg="black", height=500, width=500)
+
+couleur = tk.Button(racine, text="Choisir une couleur", font=("helvetica", "11"))
+cercle = tk.Button(racine, text="Cercle", font=("helvetica", "11"))
+carre = tk.Button(racine, text="Carré", font=("helvetica", "11"))
+croix = tk.Button(racine, text="Croix", font=("helvetica", "11"))
+
+canvas.grid(row= 7, column= 3)
+couleur.grid(row= 0, column= 3)
+cercle.grid(row= 3, column= 0)
+carre.grid(row= 5, column= 0)
+croix.grid(row= 6, column= 0)
+
+
+canvas.create_oval(x - 50, y0 + 50, x + 50, y0 - 50)
+canvas.create_rectangle()
+
+racine.mainloop()
